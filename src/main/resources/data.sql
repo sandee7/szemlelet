3ccf76222fdc9b0901b6ea29c6bc2c7868c92953
Patrik <Patrik@192.168.0.8>
2018-12-09 21:47:25 +0100

    Added more products and users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,20 @@ insert into category(category_name,created_at,updated_at) values ('elsokat', CUR
 insert into category(category_name,created_at,updated_at) values ('masodikkat', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 insert into category(category_name,created_at,updated_at) values ('harmadikkat', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product1','description1',3000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product2','description2',4000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (2,'product3','description3',5000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product1','description1',1000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product2','description2',2000,false,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (2,'product3','description3',3000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product4','description4',4000,false,true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product5','description5',5000,true,true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product6','description6',6000,true,false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into product(category_id,product_name,description,price,is_spicy,is_vegetarian, created_at, updated_at) values (1,'product7','description7',7000,true,true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 insert into employee(username,password, enabled, role) values ('owner', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_OWNER');
-insert into employee(username,password, enabled, role) values ('employee', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
+insert into employee(username,password, enabled, role) values ('employee1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
+insert into employee(username,password, enabled, role) values ('employee2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
+insert into employee(username,password, enabled, role) values ('employee3', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
+insert into employee(username,password, enabled, role) values ('employee4', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
+insert into employee(username,password, enabled, role) values ('employee5', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_EMPLOYEE');
 
 insert into purchase(product_id,order_name,address,phone_number,is_delivered, created_at, updated_at) values (1,'prder1','proba1','030312342',false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 insert into purchase(product_id,order_name,address,phone_number,is_delivered, created_at, updated_at) values (2,'prder2','proba2','230312342',false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
